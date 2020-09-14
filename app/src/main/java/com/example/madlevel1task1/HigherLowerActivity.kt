7a25c04ff3fc26aa4d572e65b98deb620e6bc14c
Stijn <stijn.limonard@hva.nl>
2020-09-14 10:39:29 +0200

    code voor shit

--- app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
+++ app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
@@ -2,10 +2,74 @@ package com.example.madlevel1task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.madlevel1task1.databinding.ActivityHigherLowerBinding
+import kotlinx.android.synthetic.main.activity_higher_lower.*
 
 class HigherLowerActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHigherLowerBinding
+    private var currentThrow: Int = 1
+    private var lastThrow: Int = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityHigherLowerBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_higher_lower)
+    }
+
+    fun updateUI(){
+        binding.lastThrowText.text = getString(R.string.last_throw, lastThrow)
+    }
+
+    private fun rollDice() {
+        lastThrow = currentThrow
+        currentThrow = (1..6).random()
+        updateUI()
+    }
+
+    private fun onHigherClick() {
+        rollDice()
+        if(lastThrow > currentThrow){
+            onAnswerCorrect()
+        }else{
+            onAnswerIncorrect()
+        }
+    }
+
+    /**
+     * Calls [rollDice] and checks if the answer is correct.
+     */
+    private fun onLowerClick() {
+        rollDice()
+        if(lastThrow < currentThrow){
+            onAnswerCorrect()
+        }else{
+            onAnswerIncorrect()
+        }
+    }
+
+    /**
+     * Calls [rollDice] and checks if the answer is correct.
+     */
+    private fun onEqualClick() {
+        rollDice()
+        if(lastThrow == currentThrow){
+            onAnswerCorrect()
+        }else{
+            onAnswerIncorrect()
+        }
+    }
+
+    private fun onAnswerCorrect() {
+        //Implement this function yourself!
+        Toast.makeText(this, getString(R.string.correctanswer),Toast.LENGTH_SHORT).show()
+    }
+
+    /**
+     * Displays an incorrect Toast message.
+     */
+    private fun onAnswerIncorrect() {
+        Toast.makeText(this, getString(R.string.incorrectanswer),Toast.LENGTH_SHORT).show()
     }
 }
